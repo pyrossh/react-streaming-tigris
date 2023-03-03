89c5f12489192c8b224f139f31e687ddf1b49ad1
Peter John <peter.john@pyros.sh>
2023-03-04 01:46:24 +0530

    Add eslint

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import path from 'path'
-import { telefunc } from 'telefunc/vite'
-import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
+import path from 'path';
+import { telefunc } from 'telefunc/vite';
+import react from '@vitejs/plugin-react';
+import ssr from 'vite-plugin-ssr/plugin';
+import eslint from 'vite-plugin-eslint';
 
 export default {
-  plugins: [react(), ssr(), telefunc()],
+  plugins: [react(), ssr(), telefunc(), eslint()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname),
